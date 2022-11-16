2613cf28cfba03cd6ceda595819727a4d39fd0a9
rperciaccante <Bob@perciaccante.net>
2022-11-16 17:48:28 -0500

    Create tft_espi_display.h

---
+++ tft_espi_display.h
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include "esphome.h"
+#include "Free_Fonts.h"
+
+
+#ifndef DISPLAYCONTROLLER
+#define DISPLAYCONTROLLER
+
+
+class DisplayController : public Component
+
+{
+	public:
+
+	void setup() override {
+		tft.init();
+	}
+	TFT_eSPI tft = TFT_eSPI();
+	TFT_eSprite spr = TFT_eSprite(&tft); // Sprite object
+	private:
+};
+DisplayController * display = new DisplayController();
+
+#endif
